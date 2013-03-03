444e876bad2c1366047fd05162af20731c9c5287
pavel <pavel@DNS-Home-XXL.(none)>
2013-03-04 02:59:59 +0400

    fixed some changes

--- gold.cpp
+++ gold.cpp
@@ -1,29 +1,48 @@
-#include <iostream.h>
+// Решение задачи оптимизации полинома 3-го порядка
+// программа находит минимальное значение функции на отрезке
+
+#include <iostream>
 #include <math.h>
 using namespace std;
 
 double f(double x);
+double A,B,C,D,a,b;
 
-void main()
+int main(void)
 {
 double tau=(sqrt(5.)-1.)/2.;
-double eps=1e-7;
-double a=-0.5
-double b=0.5;
+double eps=pow(10,-6);
+
+
+cout<<"Введите A: ";
+cin>>A;
+cout<<"Введите B: ";
+cin>>B;
+cout<<"Введите C: ";
+cin>>C;
+cout<<"Введите D: ";
+cin>>D;
+cout<<"Введите нижнюю границу изменения x (минимум): ";
+cin>>a;
+cout<<"Введите верхнюю границу изменения x (максимум): ";
+cin>>b;
+
 double x1,x2;
 while (b-a>eps)
     {
     x1=b-tau*(b-a);
     x2=a+tau*(b-a);
-    if (f(x1)>f(x2))
+    if (f(x1)>=f(x2))
        b=x2;
     else
        a=x1; 
     }
-cout<<x1;
+cout<<"Максимум равен: "<<f(x1)<<"\n";
+
+return 0;
 }
 
 double f(double x)
 {
-return 1/3*x*x*x+(1+x)*(ln(1+x)+1);
+return A*pow(x,3)+B*pow(x,2)+C*x+D;
 }
